50c67757fc9de05f96918bda8c95d51a8da31647
tfjackc <colpitt.jack@gmail.com>
2023-11-11 23:32:26 -0800

    more func

--- store/mapping.ts
+++ store/mapping.ts
@@ -3,6 +3,7 @@ import { initialize } from "~/gis/map";
 import MapView from '@arcgis/core/views/MapView';
 import FeatureSet from "@arcgis/core/rest/support/FeatureSet";
 import { surveyLayer, graphicsLayer, simpleFillSymbol, surveyTemplate } from "~/gis/layers";
+import Graphic from "@arcgis/core/Graphic";
 
 let view: MapView;
 let featureSetData: FeatureSet
@@ -51,8 +52,56 @@ export const useMappingStore = defineStore('mapping_store', {
             queryLayer.returnQueryGeometry = true;
 
             return layer.queryFeatures(queryLayer).then((fset: any) => {
-                console.log(fset);
+                this.createGraphicLayer(fset);
             });
+        },
+
+        async createGraphicLayer(fset: any) {
+            if (fset && fset.features) {
+                const promises = fset.features.map(async (survey: any) => {
+                    const graphic = new Graphic({
+                        geometry: survey.geometry,
+                        attributes: survey.attributes,
+                        symbol: simpleFillSymbol,
+                        popupTemplate: surveyTemplate
+                    });
+
+                    graphicsLayer.graphics.push(graphic);
+                    view.map.add(graphicsLayer);
+
+                    return survey.attributes;
+                });
+                // Use Promise.all to wait for all promises to resolve
+                const surveyAttributesArray = await Promise.all(promises);
+                // Add the survey attributes to the filteredData array
+                this.filteredData.push(...surveyAttributesArray);
+                // Calculate the extent of all graphics
+                const graphicsExtent = fset.features.reduce((extent: any, survey: any) => {
+                    extent.union(survey.geometry.extent);
+                    return extent;
+                }, fset.features[0].geometry.extent);
+
+                view.goTo(graphicsExtent).then(() => {
+                    console.log("view.GoTo Searched Surveys");
+                });
+            } else {
+                console.warn('No features found in the query result.');
+            }
+        },
+
+        async clearSurveyLayer() {
+            surveyLayer.visible = false
+            this.surveyLayerCheckbox = false
+        },
+
+        async surveyLayerCheck(e: any){
+            this.surveyLayerCheckbox = e.target.checked;
+            surveyLayer.visible = this.surveyLayerCheckbox;
+        },
+
+        async searchedLayerCheck(e: any) {
+            this.searchedLayerCheckbox = e.target.checked;
+            graphicsLayer.visible = this.searchedLayerCheckbox;
         }
 
     }
